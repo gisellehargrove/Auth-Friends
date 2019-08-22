47a13dc17b870d1cf4d43500fedb8cc071de940d
gisellehargrove <gisellehargrove@gmail.com>
2019-08-22 09:59:59 -0400

    displaying friends

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const FriendsList = () => {
+  const [friendsList, setFriendsList] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth().get('http://localhost:5000/api/friends')
+      .then(response => {
+        console.log(response);
+        setFriendsList(response.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, []);
+
+
+  if(!friendsList.length) return <h1>...Loading</h1>
 
 
   return (
-    <div>Friends List Component</div>
+    <div>
+      {friendsList.map(friend =>
+        <div className='friend-container' key={friend.id}>
+          <div className='name'>
+            {friend.name}
+          </div>
+          <div className='age'>
+            {friend.age}
+          </div>
+
+          <div className='email'>
+            {friend.email}
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
