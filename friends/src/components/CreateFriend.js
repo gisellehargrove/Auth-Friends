6c2b17f8774779c0451f93e827b67cc346de488e
gisellehargrove <gisellehargrove@gmail.com>
2019-08-22 10:14:38 -0400

    creating new friend

--- friends/src/components/CreateFriend.js
+++ friends/src/components/CreateFriend.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const CreateFriend = () => {
+  const [creds, setCreds] = useState({
+    name: '',
+    age: '',
+    email: ''
+  })
+
+  const handleSubmit = e => {
+    axiosWithAuth().post('http://localhost:5000/api/friends', creds)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  const handleChange = e => {
+    setCreds(
+      {
+        ...creds,
+        [e.target.name]: e.target.value
+      }
+    );
+  };
 
 
   return (
-    <div>Create Friend Component</div>
+    <div>
+      <form onSubmit={e => handleSubmit(e)}>
+        <input placeholder='name' onChange={e => handleChange(e)} name='name' value={creds.name} />
+        <input placeholder='age' onChange={e => handleChange(e)} name='age' value={creds.age} />
+        <input placeholder='email' onChange={e => handleChange(e)} name='email' value={creds.email} />
+        <button>Add New Friend</button>
+      </form>
+    </div>
   )
 
 };
