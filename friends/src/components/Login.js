00b4ec46d1f154e32b0da2e3bc530672799f5197
gisellehargrove <gisellehargrove@gmail.com>
2019-08-22 09:09:31 -0400

    fix errors

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 
 const Login = props => {
 
+  const [credentials, setCredentials] = useState(
+    {
+      credentials: {
+        username: '',
+        password: ''
+      }
+    })
+
+  const handleChange = e => {
+    setCredentials(
+      { credentials:
+        {
+          ...credentials.credentials,
+          [e.target.name]: e.target.value
+        }
+      })
+  };
+
+  const login = e => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/api/login', credentials)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  };
+
 
   return (
     <form>
-      <input placeholder='username' />
-      <input placeholder='password' />
+      <input onChange={ e => handleChange(e) } placeholder='username' />
+      <input onChange={ e => handleChange(e) } placeholder='password' />
       <button type='submit'> Login </button>
     </form>
   )
