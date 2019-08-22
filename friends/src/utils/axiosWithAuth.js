897deb09a80bf739680601cd53daa4cb5cc25494
gisellehargrove <gisellehargrove@gmail.com>
2019-08-22 09:36:33 -0400

    created private route component

--- friends/src/utils/axiosWithAuth.js
+++ friends/src/utils/axiosWithAuth.js
@@ -4,4 +4,11 @@ import axios from 'axios';
 export const axiosWithAuth = () => {
   const token = localStorage.getItem('userToken');
 
+  return axios.create({
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${token}`
+    }
+  });
+
 };
