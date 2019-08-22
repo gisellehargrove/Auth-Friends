e9b60601acf3adcd9fad7dd0f777be890c71d8b5
gisellehargrove <gisellehargrove@gmail.com>
2019-08-22 10:06:18 -0400

    added header links to navigate between states

--- friends/src/App.js
+++ friends/src/App.js
@@ -6,14 +6,18 @@ import { Route } from 'react-router-dom';
 import Login from './components/Login';
 import FriendsList from './components/FriendsList';
 import PrivateRoute from './components/PrivateRoute';
+import CreateFriend from './components/CreateFriend';
+import Header from './components/Header';
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
       <Route path='/login' component={ Login } />
-      
+
       <PrivateRoute exact path='/' component={ FriendsList } />
+      <PrivateRoute path='/create' component={ CreateFriend } />
 
 
     </div>
---
+++ friends/src/components/CreateFriend.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CreateFriend = () => {
+
+
+  return (
+    <div>Create Friend Component</div>
+  )
+
+};
+
+export default CreateFriend;
